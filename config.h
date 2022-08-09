351c79ea2a3d98bdd7a4a794678729d31ffcdd82
dlnoy <dylanouldyounes@gmail.com>
2022-08-09 18:43:12 +0200

    toz

--- config.h
+++ config.h
@@ -9,26 +9,14 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char col_urgborder[]   = "#ff0000";
-
-static const char *colors[][3]      = {
-       /*               fg         bg         border   */
-       [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-       [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-       [SchemeUrg]  = { col_gray4, col_cyan,  col_urgborder  },
- };
+#include "/home/sunny/.cache/wal/colors-wal-dwm.h"
 
 typedef struct {
        const char *name;
        const void *cmd;
 } Sp;
 const char *spcmd1[] = {"st", "-n", "spterm", "-g", "120x34", NULL };
-const char *spcmd2[] = {"st", "-n", "spfm", "-g", "144x41", "-e", "ranger", NULL };
+const char *spcmd2[] = {"st", "-n", "spfm", "-g", "144x41", "-e", "lfrun", NULL };
 const char *spcmd3[] = {"st", "-n", "spterm2", "-g", "120x34", NULL };
 static Sp scratchpads[] = {
        /* name          cmd  */
@@ -85,7 +73,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+
+static const char *dmenucmd[] = { /* "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", color0, "-nf", color15, "-sb", color1,"-sf", color15, NULL*/ "/home/sunny/dmen.sh", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 
